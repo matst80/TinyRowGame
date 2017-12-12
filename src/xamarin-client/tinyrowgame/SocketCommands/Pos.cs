082a5e3d375cd3e5fc50e9d3cf4f9ec1e78a47be
mats tornberg <mats.tornberg@gmail.com>
2017-12-12 23:13:16 +0100

    messages

---
+++ src/xamarin-client/tinyrowgame/SocketCommands/Pos.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace tinyrowgame.SocketCommands
+{
+    public class Pos
+    {
+        [JsonProperty("x")]
+        public int X { get; set; }
+
+        [JsonProperty("y")]
+        public int Y { get; set; }
+
+        [JsonProperty("value")]
+        public int Value { get; set; }
+
+    }
+}
